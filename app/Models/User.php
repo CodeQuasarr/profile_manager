6206b56f522ef5243885792f890e064e37c60232
Samuel ANYARONKE <dsanyaronke@gmail.com>
2024-08-07 06:43:30 +0200

    issue #1 : ajout des fields dans le modèle User; Init des role et permission des users
    Utiliser une table distincte pour les administrateurs ne sera pas une bonne idée dans ce contexte :
    Complexité accrue : Gérer plusieurs tables pour différents types d'utilisateurs peut augmenter la complexité du système.
    Duplication des données : Dupliquer des colonnes et des logiques qui sont communes à tous les utilisateurs ( par exemple nom, prénom, mot de passe )

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,18 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes, HasRoles;
+
+    public const STATUS_ACTIVE = '3';
+    public const STATUS_PENDING = '2';
+    public const STATUS_INACTIVE = '1';
 
     /**
      * The attributes that are mass assignable.
@@ -17,9 +23,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'first_name', 'last_name', 'email', 'password', 'status', 'image',
     ];
 
     /**
@@ -42,6 +46,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'status' => 'integer',
         ];
     }
 }
